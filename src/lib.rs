b5a1e61c8f1d95de958a68bfac4b2792809b99e0
Dylan Gallagher <gallagdy@tcd.ie>
2024-08-10 09:43:00 +0200

    Added docstrings and  method

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
+#[derive(Debug)]
+pub enum PoolCreationError {
+    ZeroSizeError,
+}
+
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of threads in the pool
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
         ThreadPool
     }
 
@@ -9,4 +22,12 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static
     {}
+
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size == 0 {
+            return Err(PoolCreationError::ZeroSizeError)
+        }
+
+        Ok(ThreadPool::new(size))
+    }
 }
